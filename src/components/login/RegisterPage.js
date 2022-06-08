b210baadb04ef01ddf253094e91e564c59d73db5
devisarapi <darapi@rpay.ai>
2022-06-08 05:27:10 +0200

    changed field name

--- src/components/login/RegisterPage.js
+++ src/components/login/RegisterPage.js
@@ -106,7 +106,7 @@ export default function SignUp(props) {
                                     required
                                     fullWidth
                                     id="username"
-                                    label="First Name"
+                                    label="Username"
                                     autoFocus
                                 />
                             </Grid>
