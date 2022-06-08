1e59abd355ecd7a2403af935e91df8125c9d0d45
devisarapi <darapi@rpay.ai>
2022-06-08 04:05:05 +0200

    change

--- src/config.js
+++ src/config.js
@@ -4,6 +4,3 @@ export const BASE_URL = "https://homework-app-bukvyscdpq-ew.a.run.app/api";
 // ENDPOINTS - auth
 export const SIGN_IN = "/auth/signin";
 export const SIGN_UP = "/auth/signup";
-// export const FORGET_PASSWORD = "/api/v1/auth/forgot-password";
-// export const USER_LOGGED_IN = "/api/v1/auth/me";
-// export const LOGOUT = "/api/v1/auth/logout";
