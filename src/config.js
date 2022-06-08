f73cac0b290c59b2dcccfecc3af333ee0cae6617
devisarapi <darapi@rpay.ai>
2022-06-08 03:53:30 +0200

    config change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // SERVER URL & STORAGE
-export const BASE_URL = "http://192.168.1.41:8080/api";
+export const BASE_URL = "https://homework-app-bukvyscdpq-ew.a.run.app/api";
 
 // ENDPOINTS - auth
 export const SIGN_IN = "/auth/signin";
